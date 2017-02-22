de5abecc1d31129096d208c868752786a4476af2
Aleksei Piatetskyi <a.piatetskyi@gmail.com>
2017-02-22 12:31:48 +0200

    Stylized index.html page

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function initMap() {
+
+	var map = new google.maps.Map(document.getElementById('google-map'), {
+		center: {lat: 34.737427, lng: -111.761907},
+		scrollwheel: false,
+		zoom: 9,
+		disableDefaultUI: true
+	});
+
+	var marker = new google.maps.Marker({
+    position: {lat: 34.867427, lng: -111.761907},
+    map: map,
+    title: 'Sedona Reserve'
+  });
+}
