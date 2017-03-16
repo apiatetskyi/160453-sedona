ad182ee59c7fbe57335cd1481fc53f9adb2ef60c
Aleksei Piatetskyi <a.piatetskyi@gmail.com>
2017-03-16 19:32:06 +0200

    normalize.css default settings, google maps dom-elem checking, fixed IE 10/11 bugs

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 'use strict'
 
 function initMap() {
+  var mapElem = document.getElementById('google-map');
 
-	var map = new google.maps.Map(document.getElementById('google-map'), {
-		center: {lat: 34.737427, lng: -111.761907},
-		scrollwheel: false,
-		zoom: 9,
-		disableDefaultUI: true
-	});
+  if (mapElem) {
+    var map = new google.maps.Map(mapElem, {
+      center: {lat: 34.737427, lng: -111.761907},
+      scrollwheel: false,
+      zoom: 9,
+      disableDefaultUI: true
+    });
 
-	var marker = new google.maps.Marker({
-		position: {lat: 34.867427, lng: -111.761907},
-		map: map,
-		title: 'Sedona Reserve'
-	});
+    var marker = new google.maps.Marker({
+      position: {lat: 34.867427, lng: -111.761907},
+      map: map,
+      title: 'Sedona Reserve'
+    });
+  }
 }
